83419158b4619c6470df83845897087f4c18755d
dannysbtz <zambranodiazjosedaniel@gmail.com>
2022-08-20 12:13:13 -0500

    javascript2

--- javascript.js
+++ javascript.js
@@ -1,3 +1,35 @@
-const title = document.getElementById('title');
-console.log(title);
-title.innerHTML = 'New name';
+const input = document.querySelector('.center');
+console.log(input.nextElementSibling);
+
+const texto = document.getElementById('title');
+texto.textContent = 'hola mundo';
+
+//console.log(title.parentElement);
+
+const divs = document.getElementsByTagName('div');
+
+
+//for(var i=0;i<divs.length;i++){
+//    if(i % 3 == 0)
+//        divs[i].style.background = 'black';
+//}
+
+const inputs= document.querySelectorAll('.input input');
+for(var i=0;i<inputs.length;i++){
+    inputs[i].addEventListener('click',backblack(i));
+    inputs[i].addEventListener('blur',backblur(i));
+}
+
+const boton = document.getElementById('button');
+boton.addEventListener('dblclick',hola);
+
+function hola(){
+    console.log("si jalo esta madre");
+}
+
+function backblack(num){
+    inputs[num].style.background = 'black';
+}
+function backblur(num){
+    inputs[num].style.background = 'white';
+}
